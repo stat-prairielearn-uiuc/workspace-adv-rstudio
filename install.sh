0666dcdb5fc794414c23b49aaaeeb4a7f5ba416a
James J Balamuta <coatless@users.noreply.github.com>
2022-04-07 21:58:37 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+echo "[config] Checking python version ..."
+python --version
+
+echo "[install] Python 3.8 ..." 
+yum install -y python38 python38-devel
+
+alternatives --set python /usr/bin/python3.8
+
+echo "[config] Checking old python version post-install ..."
+python --version
+
+echo "[config] Checking new python version post-install ..."
+python38 --version
+
 echo "[install] latest pip version ..."
 pip install --upgrade pip
 
